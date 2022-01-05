53ac3da5311fbfa74612636b771cf37ee42ffafa
Michał Lubczyński <72343815+michalubczynski@users.noreply.github.com>
2022-01-05 20:41:49 +0100

    Update decrease and increase resist

--- ARDUINO_PROGRAM/ARDUINO_PROGRAM.ino
+++ ARDUINO_PROGRAM/ARDUINO_PROGRAM.ino
@@ -1,24 +1,45 @@
 #define cellPin A1
 float counts=0;
 float miliVolts=0;
-const float mvToCount=4.8828125;
+const float mvToCount=4.8828125;   //  (5V/1024)=0.0048828125
+const int StepX =2;
+const int DirX=5;
+int stepsPerRevolution=2048;
+int motSpeed=10;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-
+pinMode(StepX,OUTPUT);
+pinMode(DirX,OUTPUT);
 } 
 
 void loop() {
-  // put your main code here, to run repeatedly:
- counts = analogRead(cellPin);
- //Serial.println(String(cellPin));
- //  (5V/1024)=0.0048828125
-if(counts<274){
- miliVolts=counts*mvToCount*0.001;
- Serial.print(String(miliVolts));
-// Serial.println(" Volts");
- delay(1000);
-}
- 
 
-}
+  if(Serial.available()){
+    char skret=Serial.read();
+    obrot(skret);
+    }
+ 
+ }
+void obrot(char m_skret){
+  if(m_skret == 'p'){
+      digitalWrite(DirX,HIGH); // Enables the motor to move in a particular direction
+      for(int x = 0; x < 200; x++) {
+      digitalWrite(StepX,HIGH); 
+      delayMicroseconds(500); 
+      digitalWrite(StepX,LOW); 
+      delayMicroseconds(500); 
+      }
+    }
+    else if(m_skret == 'l'){
+              digitalWrite(DirX,LOW); //Changes the rotations direction
+        for(int x = 0; x < 200; x++) {
+        digitalWrite(StepX,HIGH);
+        delayMicroseconds(500);
+        digitalWrite(StepX,LOW);
+        delayMicroseconds(500);
+        }
+      }
+  }
+ 
